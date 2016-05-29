f4c324ef0c2eb21592471bb34edb7c44bc9c6b8a
Nishadburhan <nishadburhanofficial@gmail.com>
2016-05-29 14:29:50 +0400

    modified gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,14 @@
 module.exports = function(grunt) {
-	console.log('working');
-}
+grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.initConfig({
+	  uglify: {
+	    my_target: {
+	      files: {
+	        'dest/app.min.js': ['assets/angular/app.js']
+	      }
+	    }
+	  }
+	});
+	
+	grunt.registerTask('default');
+};

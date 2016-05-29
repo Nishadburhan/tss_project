e5d0b8b4f47b9aa81d4eb43d05ed3c9ee8d4cadf
Nishadburhan <nishadburhanofficial@gmail.com>
2016-05-29 16:43:39 +0400

    installed uglify,cssmin,watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,23 @@
 module.exports = function(grunt) {
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.initConfig({
 	  uglify: {
 	    my_target: {
 	      files: {
-	        'dest/app.min.js': ['assets/angular/app.js']
+	        'dist/app.min.js': ['assets/angular/app.js']
 	      }
 	    }
-	  }
+	  },
+	  cssmin: {
+		  target: {
+		    files: {
+		      'dist/app.min.css': ['assets/bootstrap/css/bootstrap..css']
+		    }
+		  }
+		}
 	});
 	
-	grunt.registerTask('default');
+	grunt.registerTask('default',['uglify','cssmin']);
 };
